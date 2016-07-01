88d3ebf62e72f086480109d17a7050dcaa2c95ee
Dmitriy Balakin <Dmitriy.Balakin@0x0000.ru>
2016-07-02 02:27:26 +0300

    New thread-safe stopwatch class

---
+++ src/main/java/org/libsmith/anvil/Stopwatch.java
@@ -0,0 +1,178 @@
+package org.libsmith.anvil;
+
+import org.libsmith.anvil.time.TimePeriod;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.Immutable;
+import javax.annotation.concurrent.ThreadSafe;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import static org.libsmith.anvil.TimeSource.MILLIS_TIME_SOURCE;
+
+/**
+ * @author Dmitriy Balakin <dmitriy.balakin@0x0000.ru>
+ * @created 22.06.16
+ */
+@Immutable
+@ThreadSafe
+public class Stopwatch implements Serializable {
+
+    private static final long serialVersionUID = 6703242737042402435L;
+    private static final TimeSource DEFAULT_TIME_SOURCE = MILLIS_TIME_SOURCE;
+
+    private final @Nullable String taskName;
+    private final long originTime;
+    private final TimeSource timeSource;
+
+    protected Stopwatch(@Nullable String taskName, long originTime, TimeSource timeSource) {
+        this.originTime = originTime;
+        this.timeSource = timeSource;
+        this.taskName = taskName;
+    }
+
+    public long getOriginTime() {
+        return originTime;
+    }
+
+    public TimeSource getTimeSource() {
+        return timeSource;
+    }
+
+    public @Nullable String getTaskName() {
+        return taskName;
+    }
+
+    public @Nonnull Sample sample() {
+        TimeSource timeSource = getTimeSource();
+        return new Sample(timeSource.getTime() - getOriginTime(), timeSource.getResolution());
+    }
+
+    @Override
+    public String toString() {
+        return sample().toString();
+    }
+
+    @Immutable
+    @ThreadSafe
+    public class Sample extends TimePeriod {
+
+        private static final long serialVersionUID = 3810523489112745052L;
+
+        protected Sample(long duration, TimeUnit timeUnit) {
+            super(duration, timeUnit);
+        }
+
+        public Stopwatch getOrigin() {
+            return Stopwatch.this;
+        }
+
+        @Override
+        public String toString(TimeUnit timeUnit) {
+            return taskName == null ? super.toString(timeUnit) : taskName + " " + super.toString(timeUnit);
+        }
+    }
+
+    public static Stopwatch start() {
+        return new Stopwatch(null, DEFAULT_TIME_SOURCE.getTime(), DEFAULT_TIME_SOURCE);
+    }
+
+    public static Stopwatch start(@Nonnull String taskName) {
+        return new Stopwatch(taskName, DEFAULT_TIME_SOURCE.getTime(), DEFAULT_TIME_SOURCE);
+    }
+
+    public static Stopwatch start(@Nonnull String taskName, @Nonnull TimeSource timeSource) {
+        return new Stopwatch(taskName, timeSource.getTime(), timeSource);
+    }
+
+    public static Group group(@Nonnull String groupName) {
+        return new Group(groupName, DEFAULT_TIME_SOURCE);
+    }
+
+    public static Group group(@Nonnull String groupName, @Nonnull TimeSource timeSource) {
+        return new Group(groupName, timeSource);
+    }
+
+    @ThreadSafe
+    public static class Group implements Iterable<Sample> {
+
+        private final String groupName;
+        private final TimeSource timeSource;
+        private final List<Sample> samples;
+
+        private Stopwatch current;
+
+        private Group(@Nonnull String groupName, @Nonnull TimeSource timeSource) {
+            this.groupName = groupName;
+            this.timeSource = timeSource;
+            this.samples = Collections.synchronizedList(new ArrayList<>());
+        }
+
+        public String getGroupName() {
+            return groupName;
+        }
+
+        public TimeSource getTimeSource() {
+            return timeSource;
+        }
+
+        public List<Sample> getSamples() {
+            return Collections.unmodifiableList(samples);
+        }
+
+        public Group start(@Nonnull String taskName) {
+            synchronized (samples) {
+                if (current != null) {
+                    stop();
+                }
+                current = Stopwatch.start(taskName, timeSource);
+            }
+            return this;
+        }
+
+        public boolean isRunning() {
+            synchronized (samples) {
+                return current != null;
+            }
+        }
+
+        public Group stop() {
+            synchronized (samples) {
+                if (current == null) {
+                    throw new IllegalStateException();
+                }
+                samples.add(current.sample());
+                current = null;
+            }
+            return this;
+        }
+
+        public Stream<Sample> stream() {
+            return StreamSupport.stream(spliterator(), false);
+        }
+
+        @Override
+        public @Nonnull Iterator<Sample> iterator() {
+            return getSamples().iterator();
+        }
+
+        @Override
+        public String toString() {
+            return toString(getTimeSource().getResolution());
+        }
+
+        public String toString(TimeUnit timeUnit) {
+            return getGroupName() + " " + TimePeriod.sum(getSamples()) + " [" +
+                   stream().map(v -> v.toString(timeUnit)).collect(Collectors.joining("; ")) +
+                   (current != null ? "; " + current.sample().toString(timeUnit) : "") + "]";
+        }
+    }
+}
---
+++ src/test/java/org/libsmith/anvil/StopwatchTest.java
@@ -0,0 +1,108 @@
+package org.libsmith.anvil;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.libsmith.anvil.Stopwatch.Group;
+import org.libsmith.anvil.time.TimePeriod;
+import org.mockito.Mockito;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+/**
+ * @author Dmitriy Balakin <dmitriy.balakin@0x0000.ru>
+ * @created 02.07.16
+ */
+public class StopwatchTest {
+
+    private static final TimeUnit DEFAULT_RESOLUTION = TimeUnit.MICROSECONDS;
+    private static final long DEFAULT_ITERATION = 100_000;
+
+    private TimeSource mockTimeSource;
+
+    @Before
+    public void before() {
+        AtomicLong counter = new AtomicLong();
+        mockTimeSource = Mockito.mock(TimeSource.class);
+        Mockito.when(mockTimeSource.getTime()).then(i -> counter.addAndGet(DEFAULT_ITERATION));
+        Mockito.when(mockTimeSource.getResolution()).then(i -> StopwatchTest.DEFAULT_RESOLUTION);
+    }
+
+    @Test
+    public void constructTest() {
+        assertThat(Stopwatch.start().getTaskName()).isNull();
+        assertThat(Stopwatch.start().getTimeSource()).isEqualTo(TimeSource.MILLIS_TIME_SOURCE);
+        assertThat(Stopwatch.start().getOriginTime()).isGreaterThan(0);
+        assertThat(Stopwatch.start().getOriginTime()).isLessThanOrEqualTo(System.currentTimeMillis());
+        assertThat(Stopwatch.start("Test").getTaskName()).isEqualTo("Test");
+        assertThat(Stopwatch.start("Test", TimeSource.NANO_TIME_SOURCE).getTimeSource())
+                .isEqualTo(TimeSource.NANO_TIME_SOURCE);
+
+        assertThat(Stopwatch.start("Task name", mockTimeSource).toString()).isEqualTo("Task name 100ms");
+    }
+
+    @Test
+    public void getSampleTest() {
+        Stopwatch test = Stopwatch.start("Test", mockTimeSource);
+        Mockito.verify(mockTimeSource, Mockito.times(1)).getTime();
+
+        Stopwatch.Sample sampleA = test.sample();
+        Mockito.verify(mockTimeSource, Mockito.times(2)).getTime();
+        assertThat(sampleA.getOrigin()).isSameAs(test);
+        assertThat(sampleA.getTimeUnit()).isEqualTo(DEFAULT_RESOLUTION);
+        assertThat(sampleA.getDuration()).isEqualTo(DEFAULT_ITERATION);
+
+        Stopwatch.Sample sampleB = test.sample();
+        Mockito.verify(mockTimeSource, Mockito.times(3)).getTime();
+        assertThat(sampleB.getOrigin()).isSameAs(test);
+        assertThat(sampleB.getTimeUnit()).isEqualTo(DEFAULT_RESOLUTION);
+        assertThat(sampleB.getDuration()).isEqualTo(DEFAULT_ITERATION * 2);
+    }
+
+    @Test
+    @SuppressWarnings("ConstantConditions")
+    public void sampleToStringTest() {
+        assertThat(Stopwatch.start(null, mockTimeSource).sample().toString()).isEqualTo("100ms");
+        assertThat(Stopwatch.start("Test name", mockTimeSource).sample().toString()).isEqualTo("Test name 100ms");
+        assertThat(Stopwatch.start("Test name", mockTimeSource).sample().toString()).isEqualTo("Test name 100ms");
+    }
+
+
+    @Test
+    public void groupTest() {
+        assertThat(Stopwatch.group("Simple group").getTimeSource()).isEqualTo(TimeSource.MILLIS_TIME_SOURCE);
+        assertThat(Stopwatch.group("Simple group").getGroupName()).isEqualTo("Simple group");
+
+        Group group = Stopwatch.group("Group name", mockTimeSource);
+        assertThat(group.isRunning()).isFalse();
+        group.start("Task A");
+        assertThat(group.isRunning()).isTrue();
+        assertThat(group.getSamples()).isEmpty();
+        assertThat(group.stream().mapToLong(TimePeriod::getDuration).sum()).isZero();
+        group.start("Task B");
+        assertThat(group.isRunning()).isTrue();
+        assertThat(group.getSamples()).hasSize(1);
+        assertThat(group.stream().mapToLong(TimePeriod::getDuration).sum()).isEqualTo(DEFAULT_ITERATION);
+        group.stop();
+        assertThat(group.isRunning()).isFalse();
+        assertThat(group.getSamples()).hasSize(2);
+        assertThat(group.stream().mapToLong(TimePeriod::getDuration).sum()).isEqualTo(DEFAULT_ITERATION * 2);
+        group.start("Task C");
+        assertThat(group.isRunning()).isTrue();
+        assertThat(group.getSamples()).hasSize(2);
+        assertThat(group.stream().mapToLong(TimePeriod::getDuration).sum()).isEqualTo(DEFAULT_ITERATION * 2);
+        group.stop();
+        assertThat(group.isRunning()).isFalse();
+        assertThat(group.getSamples()).hasSize(3);
+        assertThat(group.stream().mapToLong(TimePeriod::getDuration).sum()).isEqualTo(DEFAULT_ITERATION * 3);
+        assertThat(group.toString()).isEqualTo("Group name 300ms [Task A 100ms; Task B 100ms; Task C 100ms]");
+
+        assertThatThrownBy(() -> Stopwatch.group("").stop()).isInstanceOf(IllegalStateException.class);
+        assertThatThrownBy(() -> Stopwatch.group("").start("").stop().stop()).isInstanceOf(IllegalStateException.class);
+    }
+
+
+}
