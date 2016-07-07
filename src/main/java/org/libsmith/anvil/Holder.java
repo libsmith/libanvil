335ba20f51932483758a9ab4fc6fc0296fc231f0
Dmitriy Balakin <Dmitriy.Balakin@0x0000.ru>
2016-07-07 13:25:36 +0300

    Add Supplier and Consumer ability to Holder

--- src/main/java/org/libsmith/anvil/Holder.java
+++ src/main/java/org/libsmith/anvil/Holder.java
@@ -1,20 +1,24 @@
 package org.libsmith.anvil;
 
 import java.util.Optional;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  * @author Dmitriy Balakin <dmitriy.balakin@0x0000.ru>
  * @created 21.03.16 3:10
  */
-public class Holder<T> {
+public class Holder<T> implements Supplier<T>, Consumer<T>{
     private T value;
 
-    public Holder()
+    protected Holder()
     { }
 
-    public Holder(T value) {
-        setValue(value);
+    public static <T> Holder<T> of(T value) {
+        Holder<T> holder = new Holder<>();
+        holder.setValue(value);
+        return holder;
     }
 
     public T getValue() {
@@ -31,5 +35,15 @@ public class Holder<T> {
 
     public <R> Optional<R> apply(Function<? super T, ? extends R> function) {
         return hasValue() ? Optional.of(function.apply(getValue())) : Optional.empty();
+    }
+
+    @Override
+    public void accept(T value) {
+        setValue(value);
+    }
+
+    @Override
+    public T get() {
+        return getValue();
     }
 }
