6352cc310350de10502a4673d10039ed35ac8ed2
Dmitriy Balakin <Dmitriy.Balakin@0x0000.ru>
2016-07-13 01:34:03 +0300

    Immutability prohibited exception

---
+++ src/main/java/org/libsmith/anvil/exception/ImmutabilityProhibitedException.java
@@ -0,0 +1,9 @@
+package org.libsmith.anvil.exception;
+
+/**
+ * @author Dmitriy Balakin <dmitriy.balakin@0x0000.ru>
+ * @created 13.07.16
+ */
+public class ImmutabilityProhibitedException extends UnsupportedOperationException {
+    private static final long serialVersionUID = -442668234748789277L;
+}
