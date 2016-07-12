05021ca04e72d40ebf9f1c5a9906872978100a8f
Dmitriy Balakin <Dmitriy.Balakin@0x0000.ru>
2016-07-12 04:56:51 +0300

    Lazy strings

--- src/main/java/org/libsmith/anvil/text/Strings.java
+++ src/main/java/org/libsmith/anvil/text/Strings.java
@@ -1,6 +1,10 @@
 package org.libsmith.anvil.text;
 
 import javax.annotation.Nonnull;
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Supplier;
 
 /**
@@ -70,6 +74,84 @@ public class Strings {
 
     public static <T extends CharSequence> LazyCharSequence<T> lazy(Supplier<T> supplier) {
         return new LazyCharSequence<>(supplier);
+    }
+
+    public static LazyCharSequence<String> lazy(CharSequence charSequence) {
+        return new LazyCharSequence<>(() -> charSequence == null ? null : charSequence.toString());
+    }
+
+    public static LazyCharSequence<String> lazy(CharSequence pattern, Object ... arguments) {
+        return new LazyCharSequence<>(() -> pattern == null ? null
+                                                            : MessageFormat.format(pattern.toString(), arguments));
+    }
+
+    public static LazyStringBuilder lazyStringBuilder() {
+        return new LazyStringBuilder();
+    }
+
+    public static class LazyStringBuilder implements Appendable, CharSequence {
+
+        private StringBuilder stringBuilder = new StringBuilder();
+        private List<Supplier<?>> suppliers = new ArrayList<>();
+
+        protected LazyStringBuilder()
+        { }
+
+        public LazyStringBuilder append(LazyCharSequence<?> supplier) {
+            return append((Supplier<?>) supplier);
+        }
+
+        public LazyStringBuilder append(Supplier<?> supplier) {
+            suppliers.add(supplier == null ? () -> "null" : supplier);
+            return this;
+        }
+
+        public LazyStringBuilder append(Object object) {
+            return append(() -> String.valueOf(object));
+        }
+
+        @Override
+        public LazyStringBuilder append(CharSequence csq) throws IOException {
+            suppliers.add(() -> csq);
+            return this;
+        }
+
+        @Override
+        public LazyStringBuilder append(CharSequence csq, int start, int end) throws IOException {
+            suppliers.add(() -> (csq == null ? "null" : csq).subSequence(start, end));
+            return this;
+        }
+
+        @Override
+        public LazyStringBuilder append(char c) throws IOException {
+            suppliers.add(() -> c);
+            return this;
+        }
+
+        @Override
+        public int length() {
+            return build().length();
+        }
+
+        @Override
+        public char charAt(int index) {
+            return build().charAt(index);
+        }
+
+        @Override
+        public CharSequence subSequence(int start, int end) {
+            return build().subSequence(start, end);
+        }
+
+        @Override
+        public @Nonnull String toString() {
+            return build().toString();
+        }
+
+        private StringBuilder build() {
+            suppliers.stream().map(Supplier::get).forEach(stringBuilder::append);
+            return stringBuilder;
+        }
     }
 
     public static class LazyCharSequence<T extends CharSequence> implements Supplier<T>, CharSequence {
