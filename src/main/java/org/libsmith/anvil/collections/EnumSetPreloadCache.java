980db37c9f5d77beddb1472e92fa8ec75931eedd
Dmitriy Balakin <Dmitriy.Balakin@0x0000.ru>
2016-08-06 03:10:49 +0300

    EnumSetPreloadCache implementation

---
+++ src/main/java/org/libsmith/anvil/collections/EnumSetPreloadCache.java
@@ -0,0 +1,59 @@
+package org.libsmith.anvil.collections;
+
+import javax.annotation.Nonnull;
+import java.util.Set;
+import java.util.function.Function;
+
+/**
+ * @author Dmitriy Balakin <balakin@0x0000.ru>
+ * @created 05.08.2016 21:47
+ */
+public class EnumSetPreloadCache<K extends Enum<K>, V> {
+
+    private static final int DEFAULT_MAX_SIZE = 128;
+
+    private final V[] cache;
+    private final Function<Set<K>, V> loader;
+    private final EnumSetPacker<K> packer;
+
+    public static <K extends Enum<K>, V> EnumSetPreloadCache<K, V> of(
+            @Nonnull Class<K> enumClass, @Nonnull Function<Set<K>, V> loader) {
+        return new EnumSetPreloadCache<>(DEFAULT_MAX_SIZE, enumClass, loader);
+    }
+
+    public static <K extends Enum<K>, V> EnumSetPreloadCache<K, V> of(
+            int maxSize, @Nonnull Class<K> enumClass, @Nonnull Function<Set<K>, V> loader) {
+        return new EnumSetPreloadCache<>(maxSize, enumClass, loader);
+    }
+
+    @SuppressWarnings("unchecked")
+    protected EnumSetPreloadCache(int maxSize, @Nonnull Class<K> enumClass, @Nonnull Function<Set<K>, V> loader) {
+
+        if (maxSize < 0) {
+            throw new IllegalArgumentException("Max size must be greater than zero, got: " + maxSize);
+        }
+
+        this.packer = EnumSetPacker.of(enumClass);
+        this.loader = loader;
+
+        int length = packer.getMaxWidth();
+        int size = length == 0 ? 0 : length > 32 ? Integer.MAX_VALUE : 1 << length - 1;
+        if (size < length || size > maxSize) {
+            size = maxSize;
+        }
+
+        V[] cache = (V[]) new Object[size];
+        for (int i = 0; i < size; i++) {
+            cache[i] = loader.apply(packer.unpack(i));
+        }
+        this.cache = cache;
+    }
+
+    public V get(Set<K> key) {
+        int index = (int) packer.packToLongInexact(key);
+        if (index < 0 || index >= cache.length || Integer.bitCount(index) < key.size()) {
+            return loader.apply(key);
+        }
+        return cache[index];
+    }
+}
---
+++ src/test/java/org/libsmith/anvil/collections/EnumSetPreloadedCacheTest.java
@@ -0,0 +1,65 @@
+package org.libsmith.anvil.collections;
+
+
+import org.junit.Test;
+import org.libsmith.anvil.collections.MockEnums.LongLongEnum;
+import org.mockito.Mockito;
+
+import java.util.EnumSet;
+import java.util.Set;
+import java.util.function.Function;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.libsmith.anvil.collections.MockEnums.LongLongEnum.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.times;
+
+/**
+ * @author Dmitriy Balakin <balakin@0x0000.ru>
+ * @created 05.08.2016 22:07
+ */
+public class EnumSetPreloadedCacheTest {
+
+    @Test
+    public void staticValuesTest() {
+        EnumSetPreloadCache<LongLongEnum, String> cache =
+                EnumSetPreloadCache.of(LongLongEnum.class, Object::toString);
+
+        assertThat(cache.get(EnumSet.of(_1, _3, _4))).isEqualTo("[_1, _3, _4]");
+        assertThat(cache.get(EnumSet.of(_0, _2, _5))).isEqualTo("[_0, _2, _5]");
+        assertThat(cache.get(EnumSet.of(_0, _22, _31))).isEqualTo("[_0, _22, _31]");
+        assertThat(cache.get(EnumSet.of(_4, _65, _127))).isEqualTo("[_4, _65, _127]");
+    }
+
+    @Test
+    public void preloadTest() {
+
+        class ToStringFunction implements Function<Set<LongLongEnum>, String> {
+            @Override
+            public String apply(Set<LongLongEnum> set) {
+                return set.toString();
+            }
+        }
+
+        Function<Set<LongLongEnum>, String> loader = Mockito.spy(new ToStringFunction());
+
+        EnumSetPreloadCache<LongLongEnum, String> cache =
+                EnumSetPreloadCache.of(40, LongLongEnum.class, loader);
+
+        Mockito.verify(loader, times(40)).apply(any());
+
+        EnumSetPacker<LongLongEnum> packer = EnumSetPacker.of(LongLongEnum.class);
+        for (int i = 0; i < 40; i++) {
+            Set<LongLongEnum> unpacked = packer.unpack(i);
+            assertThat(cache.get(unpacked)).isEqualTo(unpacked.toString());
+        }
+
+        Mockito.verify(loader, times(40)).apply(any());
+
+        for (int i = 0; i < 100_000; i++) {
+            Set<LongLongEnum> unpacked = packer.unpack(i);
+            assertThat(cache.get(unpacked)).isEqualTo(unpacked.toString());
+        }
+        Mockito.verify(loader, times(100_000)).apply(any());
+    }
+}
