62a1f7a9203e542426b83dcc3a25c84e3401da2a
Dmitriy Balakin <Dmitriy.Balakin@0x0000.ru>
2016-08-08 03:21:05 +0300

    Lost ClassReflectionTest form last push

--- src/test/java/org/libsmith/anvil/reflection/ClassReflectionTest.java
+++ src/test/java/org/libsmith/anvil/reflection/ClassReflectionTest.java
@@ -2,10 +2,12 @@ package org.libsmith.anvil.reflection;
 
 import org.junit.Test;
 import org.libsmith.anvil.AbstractTest;
+import org.libsmith.anvil.EqualityAssertions;
 import org.libsmith.anvil.reflection.ReflectiveOperationRuntimeException.NoSuchMemberRuntimeException;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.math.BigInteger;
 
 import static java.util.stream.Collectors.joining;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,26 +22,28 @@ public class ClassReflectionTest extends AbstractTest {
 
     @Test
     public void instantiationTest() {
-        assertEquals(
-                Subject.class,
-                ClassReflection.of(Subject.class).getReflectionSubjectClass());
 
         assertEquals(
                 Subject.class,
-                ClassReflection.ofInstance(new Subject()).getReflectionSubjectClass());
+                ClassReflection.of(Subject.class).getReflectionSubject());
+
+        assertEquals(
+                Subject.class,
+                ClassReflection.ofInstance(new Subject()).getReflectionSubject());
 
         assertEquals(
                 ParentSubject.class,
-                ClassReflection.ofDeclaringClass(ClassReflection.of(Subject.class).getFieldExact("PARENT_PUBLIC_FIELD"))
-                               .getReflectionSubjectClass());
+                ClassReflection.ofDeclaringClass(ClassReflection.of(Subject.class).getField("PARENT_PUBLIC_FIELD"))
+                               .getReflectionSubject());
         assertEquals(
                 ParentSubject.class,
-                ClassReflection.ofDeclaringClass(ClassReflection.of(Subject.class).getMethodExact("parentPublicMethod"))
-                               .getReflectionSubjectClass());
+                ClassReflection.ofDeclaringClass(ClassReflection.of(Subject.class).getMethod("parentPublicMethod"))
+                               .getReflectionSubject());
     }
 
     @Test
     public void allFieldsTest() {
+
         assertEquals(
                 "INTERFACE_FIELD, PARENT_PRIVATE_FIELD, PARENT_PRIVATE_STATIC_FIELD, PARENT_PUBLIC_FIELD, " +
                 "PARENT_PUBLIC_STATIC_FIELD, PRIVATE_FIELD, PRIVATE_STATIC_FIELD, PUBLIC_FIELD, PUBLIC_STATIC_FIELD",
@@ -59,6 +63,7 @@ public class ClassReflectionTest extends AbstractTest {
 
     @Test
     public void localFieldsTest() {
+
         assertEquals(
                 "PRIVATE_FIELD, PRIVATE_STATIC_FIELD, PUBLIC_FIELD, PUBLIC_STATIC_FIELD",
                 ClassReflection.of(Subject.class).localFields()
@@ -72,6 +77,7 @@ public class ClassReflectionTest extends AbstractTest {
 
     @Test
     public void allMethodsTest() {
+
         assertEquals(
                 "abstractMethod, abstractMethod, parentPrivateMethod, parentPrivateStaticMethod, " +
                 "parentPublicMethod, parentPublicStaticMethod, privateMethod, privateStaticMethod, " +
@@ -88,6 +94,7 @@ public class ClassReflectionTest extends AbstractTest {
 
     @Test
     public void localMethodsTest() {
+
         assertEquals(
                 "abstractMethod, privateMethod, privateStaticMethod, publicMethod, publicStaticMethod",
                 ClassReflection.of(Subject.class).localMethods()
@@ -102,24 +109,34 @@ public class ClassReflectionTest extends AbstractTest {
 
     @Test
     public void getMethodTest() {
-        ClassReflection<Subject> subjectReflection = ClassReflection.of(Subject.class);
-        assertEquals("parentPublicMethod", subjectReflection.getMethodExact("parentPublicMethod").getName());
-        assertEquals("privateMethod", subjectReflection.getLocalMethodExact("privateMethod").getName());
+
+        ClassReflection<Subject> reflection = ClassReflection.of(Subject.class);
+
+        assertThat(reflection.getMethodOp("parentPublicMethod")).isPresent();
+        assertThat(reflection.getMethod("parentPublicMethod").getName()).isEqualTo("parentPublicMethod");
+
+        assertThat(reflection.getLocalMethodOp("privateMethod")).isPresent();
+        assertThat(reflection.getLocalMethod("privateMethod").getName()).isEqualTo("privateMethod");
     }
 
     @Test
-    public void getMethodNoSuchMethodTst() {
+    public void getMethodNoSuchMethodTest() {
 
-        ClassReflection<Subject> subjectReflection = ClassReflection.of(Subject.class);
+        ClassReflection<Subject> reflection = ClassReflection.of(Subject.class);
 
-        assertThat(subjectReflection.getMethod("privateMethod").orElse(null)).isNull();
-        assertThat(subjectReflection.getLocalMethod("parentPublicMethod").orElse(null)).isNull();
+        assertThat(reflection.method("privateMethod").noParamsOp()).isNotPresent();
+        assertThat(reflection.getMethodOp("privateMethod")).isNotPresent();
+        assertThat(reflection.getLocalMethodOp("parentPublicMethod")).isNotPresent();
 
-        assertThatThrownBy(() -> subjectReflection.getMethodExact("privateMethod"))
+        assertThatThrownBy(() -> reflection.method("privateMethod").noParams())
                 .isInstanceOf(NoSuchMemberRuntimeException.class)
                 .hasCauseInstanceOf(NoSuchMethodException.class);
 
-        assertThatThrownBy(() -> subjectReflection.getLocalMethodExact("parentPublicMethod"))
+        assertThatThrownBy(() -> reflection.getMethod("privateMethod"))
+                .isInstanceOf(NoSuchMemberRuntimeException.class)
+                .hasCauseInstanceOf(NoSuchMethodException.class);
+
+        assertThatThrownBy(() -> reflection.getLocalMethod("parentPublicMethod"))
                 .isInstanceOf(NoSuchMemberRuntimeException.class)
                 .hasCauseInstanceOf(NoSuchMethodException.class);
     }
@@ -127,50 +144,124 @@ public class ClassReflectionTest extends AbstractTest {
     @Test
     public void getFieldTest() {
 
-        ClassReflection<Subject> subjectReflection = ClassReflection.of(Subject.class);
+        ClassReflection<Subject> reflection = ClassReflection.of(Subject.class);
 
-        assertEquals("PARENT_PUBLIC_FIELD", subjectReflection.getFieldExact("PARENT_PUBLIC_FIELD").getName());
-        assertEquals("PRIVATE_FIELD", subjectReflection.getLocalFieldExact("PRIVATE_FIELD").getName());
+        assertThat(reflection.fieldOp("PARENT_PUBLIC_FIELD")).isPresent();
+        assertThat(reflection.field("PARENT_PUBLIC_FIELD").getName()).isEqualTo("PARENT_PUBLIC_FIELD");
+        assertThat(reflection.getField("PARENT_PUBLIC_FIELD").getName()).isEqualTo("PARENT_PUBLIC_FIELD");
+        assertThat(reflection.getFieldOp("PARENT_PUBLIC_FIELD")).isPresent();
+
+        assertThat(reflection.localFieldOp("PRIVATE_FIELD")).isPresent();
+        assertThat(reflection.localField("PRIVATE_FIELD").getName()).isEqualTo("PRIVATE_FIELD");
+        assertThat(reflection.getLocalField("PRIVATE_FIELD").getName()).isEqualTo("PRIVATE_FIELD");
+        assertThat(reflection.getLocalFieldOp("PRIVATE_FIELD")).isPresent();
     }
 
     @Test
     public void getFieldNoSuchFieldTest() {
 
-        ClassReflection<Subject> subjectReflection = ClassReflection.of(Subject.class);
+        ClassReflection<Subject> reflection = ClassReflection.of(Subject.class);
 
-        assertThat(subjectReflection.getField("PRIVATE_FIELD").orElse(null)).isNull();
-        assertThat(subjectReflection.getLocalField("PARENT_PUBLIC_FIELD").orElse(null)).isNull();
+        assertThat(reflection.fieldOp("PRIVATE_FIELD")).isNotPresent();
+        assertThat(reflection.getFieldOp("PRIVATE_FIELD")).isNotPresent();
+        assertThat(reflection.getLocalFieldOp("PARENT_PUBLIC_FIELD")).isNotPresent();
 
-        assertThatThrownBy(() -> subjectReflection.getFieldExact("PRIVATE_FIELD"))
+        assertThatThrownBy(() -> reflection.field("PRIVATE_FIELD"))
                 .isInstanceOf(NoSuchMemberRuntimeException.class)
                 .hasCauseInstanceOf(NoSuchFieldException.class);
 
-        assertThatThrownBy(() -> subjectReflection.getLocalFieldExact("PARENT_PUBLIC_FIELD"))
+        assertThatThrownBy(() -> reflection.getField("PRIVATE_FIELD"))
+                .isInstanceOf(NoSuchMemberRuntimeException.class)
+                .hasCauseInstanceOf(NoSuchFieldException.class);
+
+        assertThatThrownBy(() -> reflection.getLocalField("PARENT_PUBLIC_FIELD"))
                 .isInstanceOf(NoSuchMemberRuntimeException.class)
                 .hasCauseInstanceOf(NoSuchFieldException.class);
     }
 
     @Test
-    public void getMembersWithModifiers() {
+    public void getConstructorTest() {
+
         ClassReflection<Subject> reflection = ClassReflection.of(Subject.class);
 
-        assertThat(reflection.getMethod("publicStaticMethod")).isPresent();
-        assertThat(reflection.getMethod("public static publicStaticMethod")).isPresent();
+        assertThat(reflection.constructor().noParams().getName()).isEqualTo(Subject.class.getName());
+        assertThat(reflection.getConstructor().getName()).isEqualTo(Subject.class.getName());
+        assertThat(reflection.getConstructorOp()).isPresent();
+    }
 
-        assertThat(reflection.getMethod("public static final publicStaticMethod")).isNotPresent();
-        assertThat(reflection.getMethod("private publicStaticMethod")).isNotPresent();
+    @Test
+    public void getConstructorNoSuchConstructorTest() {
 
-        assertThat(reflection.getField("PUBLIC_FIELD")).isPresent();
-        assertThat(reflection.getField("public static PUBLIC_STATIC_FIELD")).isPresent();
+        ClassReflection<Subject> reflection = ClassReflection.of(Subject.class);
 
-        assertThat(reflection.getField("private PUBLIC_FIELD")).isNotPresent();
-        assertThat(reflection.getField("volatile PUBLIC_STATIC_FIELD")).isNotPresent();
+        assertThat(reflection.constructor().paramsOp(Void.class)).isNotPresent();
+        assertThat(reflection.getConstructorOp(Void.class)).isNotPresent();
 
-        assertThatThrownBy(() -> reflection.getMethodExact("private publicStaticMethod"))
-                .isInstanceOf(NoSuchMemberRuntimeException.class);
+        assertThatThrownBy(() -> reflection.constructor().params(Void.class))
+                .isInstanceOf(NoSuchMemberRuntimeException.class)
+                .hasCauseInstanceOf(NoSuchMethodException.class);
 
-        assertThatThrownBy(() -> reflection.getFieldExact("private PUBLIC_FIELD"))
-                .isInstanceOf(NoSuchMemberRuntimeException.class);
+        assertThatThrownBy(() -> reflection.getConstructor(Void.class))
+                .isInstanceOf(NoSuchMemberRuntimeException.class)
+                .hasCauseInstanceOf(NoSuchMethodException.class);
+    }
+
+
+    @Test
+    public void methodInvocationTest() {
+
+        Subject subject = new Subject();
+        ClassReflection<Subject> reflection = ClassReflection.of(Subject.class);
+
+        assertThat(reflection.method("parentPublicStaticMethod").noParams().asStatic().invoke())
+                .isEqualTo("ppusm");
+
+        assertThat(reflection.localMethod("privateStaticMethod").noParams().accessible(true).asStatic().invoke())
+                .isEqualTo("prsm");
+
+        assertThat(reflection.method("privateStaticMethod").noParamsOp()).isNotPresent();
+
+        assertThat(reflection.method("publicMethod").noParams().withObject(subject).accessible(true).invoke())
+                .isEqualTo("pum");
+
+        assertThat(reflection.localMethod("privateMethod").noParams().withObject(subject).accessible(true).invoke())
+                .isEqualTo("prm");
+
+        assertThat(reflection.method("privateMethod").noParamsOp()).isNotPresent();
+    }
+
+    @Test
+    public void fieldAccessTest() {
+
+        Subject subject = new Subject();
+        ClassReflection<Subject> reflection = ClassReflection.of(Subject.class);
+
+        assertThat(reflection.field("PARENT_PUBLIC_STATIC_FIELD").asStatic().getValue())
+                .isEqualTo("ppusf");
+
+        assertThat(reflection.localField("PRIVATE_STATIC_FIELD").asStatic().accessible(true).getValue())
+                .isEqualTo("prsf");
+
+        assertThat(reflection.fieldOp("PRIVATE_STATIC_FIELD")).isNotPresent();
+
+        assertThat(reflection.field("PARENT_PUBLIC_FIELD").withObject(subject).getValue())
+                .isEqualTo("ppuf");
+
+        assertThat(reflection.localField("PRIVATE_FIELD").withObject(subject).accessible(true).getValue())
+                .isEqualTo("prf");
+
+        assertThat(reflection.fieldOp("PRIVATE_FIELD")).isNotPresent();
+    }
+
+    @Test
+    public void constructorInvocationTest() {
+
+        assertThat(ClassReflection.of(Subject.class).constructor().noParams().accessible(true).construct().publicMethod())
+                .isEqualTo("pum");
+
+        assertThat(ClassReflection.of(BigInteger.class).constructor().params(String.class).construct("42").intValue())
+                .isEqualTo(42);
+
     }
 
     @Test
@@ -196,53 +287,57 @@ public class ClassReflectionTest extends AbstractTest {
                                .map(Method::getName).collect(joining(", ")));
     }
 
+    @Test
+    public void equalsHashCodeTest() {
+        EqualityAssertions.assertThat(ClassReflection.of(String.class))
+                          .equalsTo(ClassReflection.of(String.class))
+                          .notEqualsTo(ClassReflection.of(Void.class));
+    }
+
     @SuppressWarnings("unused")
     private interface Interface {
         String INTERFACE_FIELD = "if";
-        void publicMethod();
+        String publicMethod();
     }
 
     @SuppressWarnings("unused")
     private abstract static class ParentSubject implements Interface {
 
         private static String PARENT_PRIVATE_STATIC_FIELD = "pprsf";
-        private        String PARENT_PRIVATE_FIELD        = "pprsf";
+        private        String PARENT_PRIVATE_FIELD        = "pprf";
         public static  String PARENT_PUBLIC_STATIC_FIELD  = "ppusf";
-        public         String PARENT_PUBLIC_FIELD         = "ppusf";
+        public         String PARENT_PUBLIC_FIELD         = "ppuf";
 
-        private static void parentPrivateStaticMethod() { }
-        public static void parentPublicStaticMethod() { }
+        private static String parentPrivateStaticMethod() { return "pprsm"; }
+        public static  String parentPublicStaticMethod()  { return "ppusm"; }
 
-        private void parentPrivateMethod() { }
-        public void parentPublicMethod() { }
+        private String parentPrivateMethod() { return "pprm"; }
+        public  String parentPublicMethod()  { return "ppum"; }
 
         abstract void abstractMethod();
     }
 
     @SuppressWarnings("unused")
     private static class Subject extends ParentSubject {
+
         private static String PRIVATE_STATIC_FIELD = "prsf";
-        private        String PRIVATE_FIELD        = "prsf";
+        private        String PRIVATE_FIELD        = "prf";
         public static  String PUBLIC_STATIC_FIELD  = "pusf";
-        public         String PUBLIC_FIELD         = "pusf";
+        public         String PUBLIC_FIELD         = "puf";
 
         static {
             int ignored = ParentSubject.PARENT_PRIVATE_STATIC_FIELD.hashCode(); // bridge method create
         }
 
-        private static void privateStaticMethod() {
-        }
+        private static String privateStaticMethod() { return "prsm"; }
+        public static  String publicStaticMethod()  { return "pusm"; }
 
-        public static void publicStaticMethod() {
-        }
+        private String privateMethod() { return "prm"; }
 
-        private void privateMethod() {
-        }
+        @Override
+        public String  publicMethod()  { return "pum"; }
 
-        @Override public void publicMethod() {
-        }
-
-        @Override void abstractMethod() {
-        }
+        @Override
+        void abstractMethod() { }
     }
 }
